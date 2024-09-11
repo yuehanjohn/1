a626dbfcc3c77d6e84becc1243c47184b80c6ee6
Yuehan john <yuehan.dai.john@gmail.com>
2024-09-11 09:20:02 +0300

    update

--- assignment 1/decry.py
+++ assignment 1/decry.py
@@ -1,24 +1,18 @@
 from Cryptodome.Cipher import AES
 
-# Shared secret key (must be the same as in the encryption script)
 key = b'Sixteen byte key'
 
-# Name of the file to decrypt
 filename = 'encrypted_message.bin'
 
-# Read the file
 with open(filename, 'rb') as file:
     iv = file.read(16)
     ciphertext = file.read(-1)
 
-# Split ciphertext and tag
 tag = ciphertext[-16:]
 ciphertext = ciphertext[:-16]
 
-# Create the cipher
 cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
 
-# Decrypt the message
 try:
     plaintext = cipher.decrypt_and_verify(ciphertext, tag)
     print("Decrypted message:", plaintext.decode())
